00776e7496b493cdaea7940ce78772ae15f5e77a
Jeff Amaral <jeff.amaral@cornell.edu>
2019-01-31 11:56:52 -0800

    Add participants field to new registration types automatically

--- web/modules/custom/ilr_registrations/src/Form/RegistrationTypeForm.php
+++ web/modules/custom/ilr_registrations/src/Form/RegistrationTypeForm.php
@@ -52,6 +52,8 @@ class RegistrationTypeForm extends EntityForm {
         drupal_set_message($this->t('Created the %label Registration type.', [
           '%label' => $registration_type->label(),
         ]));
+
+        $this->addParticipantsField();
         break;
 
       default:
@@ -60,6 +62,70 @@ class RegistrationTypeForm extends EntityForm {
         ]));
     }
     $form_state->setRedirectUrl($registration_type->toUrl('collection'));
+  }
+
+  /**
+   * Adds the default participants field to a registration type.
+   */
+  private function addParticipantsField() {
+    $field_storage = \Drupal::entityTypeManager()->getStorage('field_storage_config')->load('registration.field_participants');
+    $field = \Drupal::entityTypeManager()->getStorage('field_config')->load('registration.' . $this->entity->id() . '.field_participants');
+
+    if (empty($field)) {
+      $field = \Drupal::entityTypeManager()->getStorage('field_config')->create([
+        'field_storage' => $field_storage,
+        'bundle' => $this->entity->id(),
+        'label' => 'Participants',
+        'settings' => [
+          'handler' => 'default:participant',
+          'handler_settings' => [
+            'target_bundles' => [
+              'default' => 'default', // @todo: Consider setting this on the Registration type form.
+            ],
+            'sort' => [
+              'field' => '_none'
+            ],
+            'auto_create' => 'false',
+            'auto_create_bundle' => '',
+          ]
+        ],
+      ]);
+      $field->save();
+
+      $form_dislay_storage = \Drupal::entityTypeManager()->getStorage('entity_form_display');
+      $form_display = $form_dislay_storage->load('registration.' . $this->entity->id() . '.default');
+
+      if (!$form_display) {
+        $form_display = $form_dislay_storage->create([
+          'targetEntityType' => 'registration',
+          'bundle' => $this->entity->id(),
+          'mode' => 'default',
+          'status' => TRUE,
+        ]);
+      }
+
+      $form_display->setComponent('field_participants', [
+        'type' => 'inline_entity_form_complex',
+        'weight' => 10,
+        'settings' => [
+          'form_mode' => 'inline',
+          'label_singular' => '',
+          'label_plural' => '',
+          'allow_new' => true,
+          'match_operator' => 'CONTAINS',
+          'override_labels' => false,
+          'collapsible' => false,
+          'collapsed' => false,
+          'allow_existing' => false,
+          'allow_duplicate' => false,
+        ],
+      ]);
+      $form_display->removeComponent('user_id');
+      $form_display->removeComponent('event_id');
+      $form_display->save();
+    }
+
+    return $field;
   }
 
 }
