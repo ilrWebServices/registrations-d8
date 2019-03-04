9c0ac3f3a73bc401bfe3c8637423253c3a035db7
Jeff Amaral <jeff.amaral@cornell.edu>
2019-03-04 08:22:17 -0800

    Streamline gulp tasks and livereload watchers

--- gulpfile.js
+++ gulpfile.js
@@ -11,31 +11,28 @@ function css() {
     .pipe(sass({ outputStyle: "compressed" })
       .on('error', sass.logError))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('web/themes/custom/union_register/css'))
-    .pipe(livereload());
+    .pipe(gulp.dest('web/themes/custom/union_register/css'));
 }
 
-function livereloadInit(done) {
-  livereload.listen({
-    'port': 35777
-  });
+function livereloadStartServer(done) {
+  livereload.listen({ 'port': 35777 });
   done();
-}
-
-function unionLivereload() {
-  return gulp
-    .src('web/libraries/union/source/components/**/*.css')
-    .pipe(livereload());
 }
 
 function watchFiles(done) {
-  console.log('Watching for .scss file changes in /scss.');
   gulp.watch('web/themes/custom/union_register/scss/**/*.scss', css);
-  gulp.watch('web/libraries/union/source/components/**/*.css', unionLivereload);
+
+  var lr_watcher = gulp.watch([
+    'web/libraries/union/source/components/**/*.css',
+    'web/themes/custom/union_register/css/style.css'
+  ]);
+
+  lr_watcher.on('change', livereload.changed);
+
   done();
 }
 
-const watch = gulp.parallel(watchFiles, livereloadInit);
+const watch = gulp.parallel(watchFiles, livereloadStartServer);
 
 exports.sass = css
 exports.default = watch
