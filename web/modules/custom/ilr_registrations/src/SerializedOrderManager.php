65098272b181681a6d4a8ac49390dc3104ba1bd4
Jeff Amaral <jeff.amaral@cornell.edu>
2019-07-16 08:59:23 -0700

    Set serialized data registration description to empty value

--- web/modules/custom/ilr_registrations/src/SerializedOrderManager.php
+++ web/modules/custom/ilr_registrations/src/SerializedOrderManager.php
@@ -128,8 +128,8 @@ class SerializedOrderManager implements SerializedOrderManagerInterface {
         "total" => (float) $item->getTotalPrice()->getNumber(),
         "discounted_total" => (float) $item->getAdjustedTotalPrice()->getNumber(),
         "registration" => [
-          "description" => "For example, a single course with 1+ participants.",
           "additional_fields" => [],
+          "description" => "",
           "product_type" => $item->bundle(),
           "course_id" => $item->getData('sf_course_id'),
           "class_id" => $item->getData('sf_class_id'),
