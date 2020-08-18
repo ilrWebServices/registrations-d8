b058e4714ab036408c573c062396a4760b92d6cc
Jeff Amaral <jeff.amaral@cornell.edu>
2020-08-18 16:49:25 -0700

    Update the salesforce-commerce incoming webhook
    The webhook now only needs a single piece of data: the Salesforce order id.
    
    From that, the queueworker will look up, on salesforce, the corresponding commerce_order id and all other order info for the customer and participants.
    
    It will then add mappings for the order, the customer-contact, the participants, and the participant-user-contacts.
    
    Also, the processor will now only attempt to retry the mapping creation if there was a connection error. If there are other errors, they will be logged and the item will be removed from the queue.
    
    Co-authored-by: Aaron Froehlich <aaronfroehlich@gmail.com>

--- web/modules/custom/ilr_registrations/src/Plugin/QueueWorker/SalesforceCommerceWebhookProcessor.php
+++ web/modules/custom/ilr_registrations/src/Plugin/QueueWorker/SalesforceCommerceWebhookProcessor.php
@@ -4,12 +4,15 @@ namespace Drupal\ilr_registrations\Plugin\QueueWorker;
 
 use Drupal\Core\Queue\QueueWorkerBase;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Logger\LoggerChannelTrait;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\salesforce\Rest\RestClientInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\salesforce_mapping\Entity\SalesforceMapping;
 use Drupal\salesforce\SObject;
 use Drupal\Core\Entity\EntityInterface;
+use Drupal\salesforce\Rest\RestException;
+use Drupal\salesforce\SelectQuery;
 
 /**
  * A Queue worker that processes data from the Salesforce Commerce webhook.
@@ -22,12 +25,24 @@ use Drupal\Core\Entity\EntityInterface;
  */
 class SalesforceCommerceWebhookProcessor extends QueueWorkerBase implements ContainerFactoryPluginInterface {
 
+  use LoggerChannelTrait;
+
+  /**
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
   protected $entityTypeManager;
 
   /**
    * @var \Drupal\salesforce\Rest\RestClientInterface
    */
   protected $sfapi;
+
+  /**
+   * The logger.
+   *
+   * @var \Psr\Log\LoggerInterface
+   */
+  protected $logger;
 
   /**
    * Constructs a new SalesforceCommerceWebhookProcessor.
@@ -41,6 +56,7 @@ class SalesforceCommerceWebhookProcessor extends QueueWorkerBase implements Cont
   public function __construct(EntityTypeManagerInterface $entity_type_manager, RestClientInterface $sfapi) {
     $this->entityTypeManager = $entity_type_manager;
     $this->sfapi = $sfapi;
+    $this->logger = $this->getLogger('ilr_registrations_webhook');
   }
 
   /**
@@ -59,35 +75,81 @@ class SalesforceCommerceWebhookProcessor extends QueueWorkerBase implements Cont
   public function processItem($data) {
     // Verify that the salesforce order matches this commerce order by loading
     // the order object from salesforce.
-    // TODO Verify that this fails if the SFID is incorrect.
-    $order_mapping = $this->entityTypeManager->getStorage('salesforce_mapping')->load('order_to_reg_order');
-    $sf_order_object = $this->sfapi->objectRead('Order__c', $data['sf_order_id']);
-    $order = $this->entityTypeManager->getStorage('commerce_order')->load($data['pos_order_id']);
-    $this->createMapping($order_mapping, $sf_order_object, $order, TRUE);
+    try {
+      $sf_order_object = $this->sfapi->objectRead('Order__c', $data['sf_order_id']);
+    }
+    catch (RestException $e) {
+      $response_body = $e->getResponseBody();
 
-    // TODO Map the salesforce payment(s) to Commerce payment(s).
-
-    // Map the salesforce customer contact to the Drupal user for the order billing profile user (not the billing profile entity itself).
-    $contact_mapping = $this->entityTypeManager->getStorage('salesforce_mapping')->load('contact_user');
-    $sf_customer_contact_object = $this->sfapi->objectRead('Contact', $data['customer']['sf_customer_contact_id']);
-    $customer_user = $this->entityTypeManager->getStorage('user')->load($data['customer']['pos_customer_id']);
-    $this->createMapping($contact_mapping, $sf_customer_contact_object, $customer_user);
-
-    // Map the salesforce participants to the Drupal participant entities
-    // and, if set, their associated user entities.
-    $participant_mapping = $this->entityTypeManager->getStorage('salesforce_mapping')->load('basic_participant');
-
-    foreach ($data['participants'] as $participant_data) {
-      $sf_participant_object = $this->sfapi->objectRead('EXECED_Participant__c', $participant_data['sf_participant_id']);
-      $participant = $this->entityTypeManager->getStorage('participant')->load($participant_data['pos_participant_id']);
-      $this->createMapping($participant_mapping, $sf_participant_object, $participant);
-
-      // If this participant was linked to a user, map that user to the salesforce contact.
-      if (!$participant->uid->isEmpty()) {
-        $sf_participant_contact_object = $this->sfapi->objectRead('Contact', $participant_data['sf_participant_contact_id']);
-        $this->createMapping($contact_mapping, $sf_participant_contact_object, $participant->uid->entity);
+      // If this was a connection error, throw a generic exception so that this
+      // item will be retried.
+      if ($response_body === NULL) {
+        throw new \Exception($e->getMessage());
+      }
+      // If there was no connection error but there was some other error (e.g.,
+      // the SF object was not found), just log it and return with no exception.
+      // This will remove this queue item so that it will NOT be retried.
+      else {
+        $this->logger->error('Incoming salesforce webhook order lookup error for @sfid: @response_body', [
+          '@sfid' => $data['sf_order_id'],
+          '@response_body' => $response_body,
+        ]);
+        return;
       }
     }
+
+    // TODO Maybe verify that $data['pos_order_id'] is the same as
+    // $sf_order_object->field('Order_ID__c']?
+
+    // Load the commerce order. Note that this Drupal entity id is stored in
+    // Salesforce in the `Order_ID__c` field on Order_c objects.
+    $order = $this->entityTypeManager->getStorage('commerce_order')->load($sf_order_object->field('Order_ID__c'));
+
+    // Map the commerce order to the salesforce order object.
+    $order_mapping = $this->entityTypeManager->getStorage('salesforce_mapping')->load('order_to_reg_order');
+    $this->createMapping($order_mapping, $sf_order_object, $order);
+
+    // TODO Maybe verify $data['customer']['sf_customer_contact_id'] is
+    // $sf_order_object->field('Purchaser__c').
+
+    // Map the salesforce customer contact to the Drupal user for the order
+    // billing profile user (not the billing profile entity itself).
+    $sf_customer_contact_object = $this->sfapi->objectRead('Contact', $sf_order_object->field('Purchaser__c'));
+    $customer_user = $order->getCustomer();
+    $contact_mapping = $this->entityTypeManager->getStorage('salesforce_mapping')->load('contact_user');
+    $this->createMapping($contact_mapping, $sf_customer_contact_object, $customer_user);
+
+    // Map the salesforce participants to the Drupal participant entities and,
+    // if set, their associated user entities.
+    // TODO Refactor if we ever use other participant types.
+    $participant_mapping = $this->entityTypeManager->getStorage('salesforce_mapping')->load('basic_participant');
+
+    // Get SF EXECED_Application__c for this Order_c.
+    $sf_application_query = new SelectQuery('EXECED_Application__c');
+    $sf_application_query->fields[] = 'Id';
+    $sf_application_query->addCondition('Order__c', "'" . $sf_order_object->id() . "'");
+    $sf_application_results = $this->sfapi->query($sf_application_query);
+
+    foreach ($sf_application_results->records() as $sf_application) {
+      // Get SF EXECED_Participant__c for this EXECED_Application__c.
+      $sf_participant_query = new SelectQuery('EXECED_Participant__c');
+      $sf_participant_query->fields = ['Id', 'POS_Participant_Id__c', 'Contact__c'];
+      $sf_participant_query->addCondition('Application__c', "'" . $sf_application->id() . "'");
+      $sf_participant_results = $this->sfapi->query($sf_participant_query);
+
+      foreach ($sf_participant_results->records() as $sf_participant) {
+        $participant = $this->entityTypeManager->getStorage('participant')->load((int) $sf_participant->field('POS_Participant_Id__c'));
+        $this->createMapping($participant_mapping, $sf_participant, $participant);
+
+        // If this participant was linked to a user, map that user to the salesforce contact.
+        if (!$participant->uid->isEmpty()) {
+          $sf_participant_contact_object = $this->sfapi->objectRead('Contact', $sf_participant->field('Contact__c'));
+          $this->createMapping($contact_mapping, $sf_participant_contact_object, $participant->uid->entity);
+        }
+      }
+    }
+
+    // TODO Map the salesforce payment(s) to Commerce payment(s).
   }
 
   /**
@@ -102,22 +164,14 @@ class SalesforceCommerceWebhookProcessor extends QueueWorkerBase implements Cont
    * @param \Drupal\Core\Entity\EntityInterface $entity
    *   A Drupal entity. E.g., commerce_order, user, or participant.
    *
-   * @param bool $error_on_dupe
-   *   When TRUE, an error will be thrown that will mark this queue item as
-   *   failed if a mapping for this salesforce id already exists.
-   *
    * @return null
    */
-  protected function createMapping(SalesforceMapping $mapping, SObject $sf_object, EntityInterface $entity, $error_on_dupe = FALSE) {
+  protected function createMapping(SalesforceMapping $mapping, SObject $sf_object, EntityInterface $entity) {
     $mapped_object_storage = $this->entityTypeManager->getStorage('salesforce_mapped_object');
+    $sf_mapped_object = $mapped_object_storage->loadBySfidAndMapping($sf_object->id(), $mapping);
+    $drupal_mapped_object = $mapped_object_storage->loadByEntityAndMapping($entity, $mapping);
 
-    // Map the salesforce order to the Commerce order.
-    $mapped_object = $mapped_object_storage->loadByProperties([
-      'salesforce_id' => (string) $sf_object->id(),
-      'salesforce_mapping' => $mapping->id(),
-    ]);
-
-    if (empty($mapped_object)) {
+    if (empty($sf_mapped_object) && empty($drupal_mapped_object)) {
       // Create the mapping.
       $mapped_object = $mapped_object_storage->create([
         'drupal_entity' => [
@@ -136,8 +190,15 @@ class SalesforceCommerceWebhookProcessor extends QueueWorkerBase implements Cont
       // mapped object.
       $mapped_object->pull();
     }
-    elseif ($error_on_dupe) {
-      throw new \Exception('Duplicate Salesforce mapping: ' . $mapping->label());
+    else {
+      // Log a notice for attempts at duplicates.
+      $this->logger->notice('Duplicate Salesforce mapping `@mapping_id`. Salesforce `@sf_object_type` ID: @sfid. Drupal `@entity_type` ID: @entityid', [
+        '@mapping_id' => $mapping->id(),
+        '@sf_object_type' => $sf_object->type(),
+        '@entity_type' => $entity->getEntityTypeId(),
+        '@sfid' => $sf_object->id(),
+        '@entityid' => $entity->id(),
+      ]);
     }
   }
 
