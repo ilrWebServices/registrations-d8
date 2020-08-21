8d1a4965abede3104158fa60b58b9c50a610604c
Jeff Amaral <jeff.amaral@cornell.edu>
2020-08-21 15:03:54 -0700

    Use a queue for the post order salesforce webhook submission

--- web/modules/custom/ilr_registrations/src/EventSubscriber/OrderCompleteSubscriber.php
+++ web/modules/custom/ilr_registrations/src/EventSubscriber/OrderCompleteSubscriber.php
@@ -4,8 +4,8 @@ namespace Drupal\ilr_registrations\EventSubscriber;
 
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Drupal\ilr_registrations\SerializedOrderManagerInterface;
-use Drupal\salesforce\Rest\RestClientInterface;
 use Drupal\Core\Logger\LoggerChannelFactoryInterface;
+use Drupal\Core\Queue\QueueFactory;
 use Drupal\state_machine\Event\WorkflowTransitionEvent;
 use Drupal\salesforce\Rest\RestException;
 
@@ -24,13 +24,6 @@ class OrderCompleteSubscriber implements EventSubscriberInterface {
   protected $serializedOrderManager;
 
   /**
-   * Rest client service.
-   *
-   * @var \Drupal\salesforce\Rest\RestClientInterface
-   */
-  protected $sfapi;
-
-  /**
    * A logger instance.
    *
    * @var \Psr\Log\LoggerInterface
@@ -38,19 +31,26 @@ class OrderCompleteSubscriber implements EventSubscriberInterface {
   protected $logger;
 
   /**
+   * The Drupal queue.
+   *
+   * @var \Drupal\Core\Queue\QueueInterface
+   */
+  protected $queue;
+
+  /**
    * Constructs a new OrderCompleteSubscriber object.
    *
    * @param \Drupal\ilr_registrations\SerializedOrderManagerInterface $serialized_order_manager
    *   The serialized order manager.
-   * @param \Drupal\salesforce\Rest\RestClientInterface $sfapi
-   *   The salesforce rest client.
    * @param \Drupal\Core\Logger\LoggerChannelFactoryInterface $logger_factory
    *   The logger channel factory.
+   * @param \Drupal\Core\Queue\QueueFactory $queue_factory
+   *   The logger channel factory.
    */
-  public function __construct(SerializedOrderManagerInterface $serialized_order_manager, RestClientInterface $sfapi, LoggerChannelFactoryInterface $logger_factory) {
+  public function __construct(SerializedOrderManagerInterface $serialized_order_manager, LoggerChannelFactoryInterface $logger_factory, QueueFactory $queue_factory) {
     $this->serializedOrderManager = $serialized_order_manager;
-    $this->sfapi = $sfapi;
     $this->logger = $logger_factory->get('ilr_registrations_webhook');
+    $this->queue = $queue_factory->get('commerce_order_to_salesforce');
   }
 
   /**
@@ -70,37 +70,13 @@ class OrderCompleteSubscriber implements EventSubscriberInterface {
     $order = $event->getEntity();
     $serialized_order = $this->serializedOrderManager->getObjectForOrder($order);
 
-    // Use the Salesforce module Rest API to post this serialized order to their
-    // webhook. This will call the correct endpoint automatically based on the
-    // default authentication provider.
-    try {
-      $response = $this->sfapi->apiCall('/services/apexrest/WebReg', $serialized_order, 'POST', TRUE);
+    // Queue the serialized order for submission to the WebReg webhook on
+    // Salesforce.
+    $queue_item_id = $this->queue->createItem($serialized_order);
 
-      $this->logger->notice('WebReg hook success for order @order_id. Response code: @response_code. Response message: @response_message', [
-        '@order_id' => $order->id(),
-        '@response_code' => $response->getStatusCode(),
-        '@response_message' => $response->getReasonPhrase(),
-      ]);
-    }
-    // Catch `RequestException`s first.
-    catch (RestException $e) {
-      $response = $e->getResponse();
-
-      $this->logger->error('WebReg hook rest error for order @order_id. Response code: @response_code. Response message: @response_message. Error message: @message', [
-        '@order_id' => $order->id(),
-        '@response_code' => $response ? $response->getStatusCode() : '?',
-        '@response_message' => $response ? $response->getReasonPhrase() : '?',
-        '@message' => $e->getMessage(),
-      ]);
-    }
-    // Another exception may be thrown, e.g. for a network error, missing
-    // OAuth credentials, invalid params, etc.
-    catch (\Exception $e) {
-      $this->logger->error('WebReg hook error for order @order_id. Error message: @message', [
-        '@order_id' => $order->id(),
-        '@message' => $e->getMessage(),
-      ]);
-    }
+    $this->logger->notice('Outgoing Salesforce WebReg webhook request queued for order @order_id', [
+      '@order_id' => $order->id(),
+    ]);
   }
 
 }
---
+++ web/modules/custom/ilr_registrations/src/Plugin/QueueWorker/CommerceOrderToSalesforceSubmitter.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Drupal\ilr_registrations\Plugin\QueueWorker;
+
+use Drupal\Core\Queue\QueueWorkerBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Logger\LoggerChannelTrait;
+// use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\salesforce\Rest\RestClientInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+// use Drupal\salesforce_mapping\Entity\SalesforceMapping;
+// use Drupal\salesforce\SObject;
+// use Drupal\Core\Entity\EntityInterface;
+use Drupal\salesforce\Rest\RestException;
+
+/**
+ * A Queue worker that submits Commerce Orders to a Salesforce webhook.
+ *
+ * @QueueWorker(
+ *   id = "commerce_order_to_salesforce",
+ *   title = @Translation("Commerce Order to Salesforce Submitter"),
+ *   cron = {"time" = 120}
+ * )
+ */
+class CommerceOrderToSalesforceSubmitter extends QueueWorkerBase implements ContainerFactoryPluginInterface {
+
+  use LoggerChannelTrait;
+
+  /**
+   * @var \Drupal\salesforce\Rest\RestClientInterface
+   */
+  protected $sfapi;
+
+  /**
+   * The logger.
+   *
+   * @var \Psr\Log\LoggerInterface
+   */
+  protected $logger;
+
+  /**
+   * Constructs a new SalesforceCommerceWebhookProcessor.
+   *
+   * @param \Drupal\salesforce\Rest\RestClientInterface $sfapi
+   *   The salesforce rest client.
+   */
+  public function __construct(RestClientInterface $sfapi) {
+    $this->sfapi = $sfapi;
+    $this->logger = $this->getLogger('ilr_registrations_webhook');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $container->get('salesforce.client')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function processItem($data) {
+    // Use the Salesforce module Rest API to post the serialized order to their
+    // webhook. This will call the correct endpoint automatically based on the
+    // default authentication provider.
+    try {
+      $sf_response = $this->sfapi->apiCall('/services/apexrest/WebReg', $data, 'POST', TRUE);
+
+      $this->logger->notice('WebReg hook success for order @order_id. Response code: @response_code. Response message: @response_message', [
+        '@order_id' => $data['order_id'],
+        '@response_code' => $sf_response->getStatusCode(),
+        '@response_message' => $sf_response->getReasonPhrase(),
+      ]);
+    }
+    // Catch `RequestException`s first.
+    catch (RestException $e) {
+      $response = $e->getResponse();
+
+      // If this was a connection error, throw a generic exception so that this
+      // item will be retried. The exception will be logged.
+      if ($response === NULL) {
+        throw new \Exception($e->getMessage());
+      }
+      // If there was no connection error but there was some other error (e.g.,
+      // the WebReg endpoint was not found or returned an error), just log it
+      // and return with no exception. This will remove this queue item so that
+      // it will NOT be retried.
+      else {
+        $this->logger->error('WebReg hook rest error for order @order_id. Response code: @response_code. Response message: @response_message. Error message: @message', [
+          '@order_id' => $data['order_id'],
+          '@response_code' => $response->getStatusCode(),
+          '@response_message' => $response->getReasonPhrase(),
+          '@message' => $e->getMessage(),
+        ]);
+      }
+    }
+  }
+
+}
