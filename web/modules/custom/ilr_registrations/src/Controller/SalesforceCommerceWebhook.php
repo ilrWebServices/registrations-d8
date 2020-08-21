64a7d4f2b309ad36f55dc03a78c5f4c8451dab72
Jeff Amaral <jeff.amaral@cornell.edu>
2020-08-21 15:05:43 -0700

    Update incoming sf commerce webhook log message

--- web/modules/custom/ilr_registrations/src/Controller/SalesforceCommerceWebhook.php
+++ web/modules/custom/ilr_registrations/src/Controller/SalesforceCommerceWebhook.php
@@ -62,7 +62,7 @@ class SalesforceCommerceWebhook extends ControllerBase {
   public function v1(Request $request) {
     $queue_item_id = $this->queue->createItem(json_decode($request->getContent(), TRUE));
 
-    $this->logger->notice('Incoming salesforce commerce webhook queued for processing: @data', [
+    $this->logger->notice('Incoming salesforce commerce webhook data queued for processing: @data', [
       '@data' => $request->getContent(),
     ]);
 
