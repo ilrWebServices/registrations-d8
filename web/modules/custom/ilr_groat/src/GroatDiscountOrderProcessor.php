bc89ae4df0251ac4cf3168785762a442c9e698e8
Jeff Amaral <jeff.amaral@cornell.edu>
2022-01-19 13:37:08 -0500

    Add recent grad pricing to single groat tickets
    Co-authored-by: Aaron Froehlich <aaronfroehlich@gmail.com>

--- web/modules/custom/ilr_groat/src/GroatDiscountOrderProcessor.php
+++ web/modules/custom/ilr_groat/src/GroatDiscountOrderProcessor.php
@@ -6,11 +6,14 @@ use Drupal\commerce_order\Entity\OrderInterface;
 use Drupal\commerce_order\OrderProcessorInterface;
 use Drupal\commerce_price\Price;
 use Drupal\commerce_order\Adjustment;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
 
 /**
 * Provides an order processor that modifies groat single ticket prices according to class year.
 */
 class GroatDiscountOrderProcessor implements OrderProcessorInterface {
+
+  use StringTranslationTrait;
 
   /**
   * {@inheritdoc}
@@ -44,21 +47,34 @@ class GroatDiscountOrderProcessor implements OrderProcessorInterface {
       foreach ($registration->participants->referencedEntities() as $participant) {
         $class_year = $participant->field_class_year->value ?? 0;
         $years_graduated = date('Y') - $class_year;
-        $participant_name = $participant->field_address->first()->getGivenName() . ' ' . $participant->field_address->first()->getFamilyName();
+        // $participant_name = $participant->field_address->first()->getGivenName() . ' ' . $participant->field_address->first()->getFamilyName();
+
+        /** @var \Drupal\commerce_price\Price $full_price */
+        $full_price = $product_variation->getPrice();
+        $adjusted_price = new Price($full_price->getNumber(), $full_price->getCurrencyCode());
 
         if ($years_graduated < 3) {
-          $adjustments[] = new Adjustment([
-            'type' => 'alumni_discount',
-            'label' => 'Price adjustment for ' . $participant_name,
-            'amount' => new Price('-240', 'USD'),
-            'source_id' => $participant->id(),
-          ]);
+          if (!$product_variation->field_new_grad_price->isEmpty()) {
+            $adjusted_price = $product_variation->field_new_grad_price->first()->toPrice();
+          }
         }
         elseif ($years_graduated < 11) {
+          if (!$product_variation->field_recent_grad_price->isEmpty()) {
+            $adjusted_price = $product_variation->field_recent_grad_price->first()->toPrice();
+          }
+        }
+
+        if (!$adjusted_price->equals($full_price)) {
+          $discount_price = $adjusted_price->subtract($full_price);
+          $currency_formatter = \Drupal::service('commerce_price.currency_formatter');
+
           $adjustments[] = new Adjustment([
             'type' => 'alumni_discount',
-            'label' => 'Price adjustment for ' . $participant_name,
-            'amount' => new Price('-190', 'USD'),
+            'label' => $this->t('Price adjustment for @year graduate to @adjusted_price', [
+              '@year' => $class_year,
+              '@adjusted_price' => $currency_formatter->format($adjusted_price->getNumber(), $adjusted_price->getCurrencyCode()),
+            ]),
+            'amount' => $discount_price,
             'source_id' => $participant->id(),
           ]);
         }
