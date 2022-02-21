03ccafeb720759e7964346a40a33f1a11319e2d1
Jeff Amaral <jeff.amaral@cornell.edu>
2022-02-21 15:42:57 -0500

    Add a view for marketing to see orders with tracked utm codes
    Since the data is stored in a 'map' field type, a simple new field formatter and an alteration of the filter were required.
    
    The formatter just dumps the data as a YAML structure. YAML was chosen because it's easier to look at.
    
    The filter was changed from 'standard' to 'string' to allow for crude filtering. It works for the purpose of this view, but probably wouldn't for other uses.

---
+++ web/modules/custom/ilr_registrations/src/Plugin/Field/FieldFormatter/MapItemFormatter.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Drupal\ilr_registrations\Plugin\Field\FieldFormatter;
+
+use Drupal\Component\Serialization\Yaml;
+use Drupal\Core\Field\FormatterBase;
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\Plugin\Field\FieldType\MapItem;
+
+/**
+ * Plugin implementation of the 'MapItem' formatter.
+ *
+ * @FieldFormatter(
+ *   id = "map_dump",
+ *   label = @Translation("Map dump"),
+ *   field_types = {
+ *     "map"
+ *   }
+ * )
+ */
+class MapItemFormatter extends FormatterBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsSummary() {
+    $summary = [];
+    $summary[] = $this->t('Displays a "map" field.');
+    return $summary;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    $element = [];
+    $yaml = new Yaml;
+
+    foreach ($items as $delta => $item) {
+      if ($item instanceof MapItem) {
+        $element[$delta] = [
+          // '#markup' => print_r($item->toArray(), TRUE),
+          // '#markup' => json_encode($item->toArray(), JSON_PRETTY_PRINT),
+          '#markup' => $yaml->encode($item->toArray()),
+          '#prefix' => '<pre>',
+          '#suffix' => '</pre>',
+        ];
+      }
+    }
+
+    return $element;
+  }
+
+}
