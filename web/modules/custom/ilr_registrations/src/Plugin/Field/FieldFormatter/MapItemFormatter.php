30fcb7eccf775d0bedb84291c26951115e3679d0
Jeff Amaral <jeff.amaral@cornell.edu>
2022-12-06 15:52:18 -0500

    Fix error on marketing orders report

--- web/modules/custom/ilr_registrations/src/Plugin/Field/FieldFormatter/MapItemFormatter.php
+++ web/modules/custom/ilr_registrations/src/Plugin/Field/FieldFormatter/MapItemFormatter.php
@@ -38,10 +38,15 @@ class MapItemFormatter extends FormatterBase {
 
     foreach ($items as $delta => $item) {
       if ($item instanceof MapItem) {
+        $item_array = $item->toArray();
+
+        // @todo Figure out how to deal with IlrOutreachDiscount objects.
+        unset($item_array['ilr_outreach_discounts']);
+
         $element[$delta] = [
           // '#markup' => print_r($item->toArray(), TRUE),
           // '#markup' => json_encode($item->toArray(), JSON_PRETTY_PRINT),
-          '#markup' => $yaml->encode($item->toArray()),
+          '#markup' => $yaml->encode($item_array),
           '#prefix' => '<pre>',
           '#suffix' => '</pre>',
         ];
