b63431d319ff217f181b4304166e1e61bf269588
Jeff Amaral <jeff.amaral@cornell.edu>
2023-12-11 16:27:30 -0500

    Always send UTM codes in serialized orders
    Even if there are no UTM codes on the order at all.

--- web/modules/custom/ilr_registrations/src/SerializedOrderManager.php
+++ web/modules/custom/ilr_registrations/src/SerializedOrderManager.php
@@ -93,7 +93,6 @@ class SerializedOrderManager implements SerializedOrderManagerInterface {
         "job_title" => $billing_profile->field_job_title->value,
         "industry" => $billing_profile->field_industry->value,
         "phone" => $billing_profile->field_phone->value,
-        // @todo Add additional customer fields as necessary.
         "additional_fields" => [],
       ],
       "payment_owner" => $order->hasField('field_payment_owner') ? $order->field_payment_owner->value : NULL,
@@ -104,15 +103,25 @@ class SerializedOrderManager implements SerializedOrderManagerInterface {
 
     // Add any stored UTM codes if they exist in the order data. See
     // OrderCreateSubscriber::onOrderCreate().
-    if ($utm_codes = $order->getData('utm_codes')) {
-      foreach ($utm_codes as $utm_name => $utm_code) {
-        $utm_name = ($utm_name === 'utm_term') ? 'utm_keyword' : $utm_name;
-        $response['customer']['additional_fields'][] = [
-          'name' => strtoupper(substr($utm_name, 0, 5)) . substr($utm_name, 5) . '__c',
-          'value' => $utm_code,
-        ];
-      }
+    $utm_codes = $order->getData('utm_codes') ?? [];
+    $utm_fields = [];
+    $utm_field_map = [
+      'UTM_Campaign__c' => 'utm_campaign',
+      'UTM_Source__c' => 'utm_source',
+      'UTM_Keyword__c' => 'utm_term', // Contacts
+      'UTM_Term__c' => 'utm_term', // Participants
+      'UTM_Medium__c' => 'utm_medium',
+      'UTM_Content__c' => 'utm_content',
+    ];
+
+    foreach ($utm_field_map as $sf_utm_field_name => $utm_param_name) {
+      $utm_fields[] = [
+        'name' => $sf_utm_field_name,
+        'value' => $utm_codes[$utm_param_name] ?? null,
+      ];
     }
+
+    $response['customer']['additional_fields'] += $utm_fields;
 
     // Process payments.
     $commerce_payments = $payment_storage->loadByProperties([
@@ -258,6 +267,7 @@ class SerializedOrderManager implements SerializedOrderManagerInterface {
             "is_cornell_employee" => $participant->hasField('field_is_cornell_employee') ? ($participant->field_is_cornell_employee->value ? 'true' : 'false') : NULL,
             "apply_to_certificate" => "",
             // @todo Add additional participant fields as necessary.
+            // 'additional_fields' => $utm_fields,
             'additional_fields' => [],
           ];
         }
